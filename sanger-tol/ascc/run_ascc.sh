1805ccf94eefe8f11573364cc40ecc6235986169
Tom Harrop <twharrop@gmail.com>
2026-03-05 14:02:32 +1100

    initial commit

---
+++ sanger-tol/ascc/run_ascc.sh
@@ -0,0 +1,69 @@
+#!/bin/bash -l
+#SBATCH --job-name=ascc
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32g
+#SBATCH --time=1-00:00:00
+#SBATCH --account=pawsey1132
+#SBATCH --partition=work
+
+module load singularity/4.1.0-slurm
+
+unset SBATCH_EXPORT
+
+# Application specific commands:
+set -eux
+
+# Set up project. Outdir must be on scratch.
+SAMPLE_ID=rSaiEqu1
+SAMPLE_VERSION="v2"
+OUTDIR="results/${SAMPLE_ID}.${SAMPLE_VERSION}"
+
+# set up nextflow
+NEXTFLOW_VERSION="25.10.4"
+NEXTFLOW_DIR="${OUTDIR}/nextflow/${NEXTFLOW_VERSION}"
+mkdir -p "${NEXTFLOW_DIR}/logs"
+
+if [ ! -f "${NEXTFLOW_DIR}/nextflow" ]; then
+    wget \
+        -O "${NEXTFLOW_DIR}/nextflow" \
+        "https://github.com/nextflow-io/nextflow/releases/download/v${NEXTFLOW_VERSION}/nextflow"
+
+    chmod 755 "${NEXTFLOW_DIR}/nextflow"
+fi
+
+export PATH="${NEXTFLOW_DIR}:${PATH}"
+printf "nextflow: %s\n" "$( readlink -f "$( which nextflow )" )"
+export NXF_HOME="${NEXTFLOW_DIR}/.nextflow"
+export NXF_CACHE_DIR="${NEXTFLOW_DIR}/cache"
+export NXF_WORK="${NEXTFLOW_DIR}/work"
+
+# set up singularity
+if [ -z "${SINGULARITY_CACHEDIR}" ]; then
+        export SINGULARITY_CACHEDIR=/software/projects/pawsey1132/tharrop/.singularity
+        export APPTAINER_CACHEDIR="${SINGULARITY_CACHEDIR}"
+fi
+
+export NXF_APPTAINER_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
+export NXF_SINGULARITY_CACHEDIR="${SINGULARITY_CACHEDIR}/library"
+
+# set up pipeline
+PIPELINE_VERSION="65b4c1d"
+PIPELINE_PARAMS=(
+        "--input" "ascc_samplesheet.csv"
+        "--outdir" "${OUTDIR}/ascc"
+        "-profile" "singularity,pawsey"
+        "-params-file" "ascc_config.yaml"
+        "-r" "${PIPELINE_VERSION}"
+        "-c" "ascc.config"
+)
+
+# run
+nextflow \
+        -log "${NEXTFLOW_DIR}/logs/nextflow_run_ascc.$(date +"%Y%m%d%H%M%S").${RANDOM}.log" \
+        run \
+        sanger-tol/ascc \
+        "${PIPELINE_PARAMS[@]}" \
+        -resume
