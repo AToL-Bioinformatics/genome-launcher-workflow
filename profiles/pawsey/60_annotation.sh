53ea36e83fb6bbf8f5f91f99af70747e092e011f
Tom Harrop <twharrop@gmail.com>
2026-09-18 15:25:34 +1000

    WIP for pawsey config

--- profiles/pawsey/60_annotation.sh
+++ profiles/pawsey/60_annotation.sh
@@ -13,14 +13,25 @@ source profiles/pawsey/lib/snakemake_env.sh
 # Setup and run
 setup_snakemake
 
+# check this here because it's annotation-specific (for now)
+export GPU_ACCOUNT="${PAWSEY_ACCOUNT:?Set PAWSEY_ACCOUNT to run the Annotation job}_gpu"
+
 # we need a custom snakemake command because the Pawsey GPU queue doesn't
 # accept the normal SBATCH arguments.
-(
-	XDG_CACHE_HOME="$(mktemp -d)" \
-		snakemake --profile profiles/pawsey \
-		--cluster-generic-submit-cmd "mkdir -p logs/slurm/{rule} && sbatch --time={resources.runtime} --partition=gpu --account=pawsey1132-gpu --gres=gpu:{resources.gpu} --job-name={rule}-smk --output=logs/slurm/{rule}/{rule}-%j.out --parsable" \
-		tiberius
-)
+XDG_CACHE_HOME="$(mktemp -d)" \
+	snakemake --profile profiles/pawsey \
+	--cluster-generic-submit-cmd "\
+			mkdir -p logs/slurm/{rule} \
+			&& \
+			sbatch \
+			--time={resources.runtime} \
+			{resources.partitionFlag} \
+			--account=${GPU_ACCOUNT} \
+			--gres=gpu:{resources.gpu} \
+			--job-name={rule}-smk \
+			--output=logs/slurm/{rule}/{rule}-%j.out \
+			--parsable" \
+	tiberius
 
 exit 0
 
