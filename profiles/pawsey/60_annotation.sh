a30a602c52d8c5601d2adb8da7677f39110f1241
Tom Harrop <twharrop@gmail.com>
2026-09-18 12:08:56 +1000

    modified submission for GPU

--- profiles/pawsey/60_annotation.sh
+++ profiles/pawsey/60_annotation.sh
@@ -12,4 +12,16 @@ source profiles/pawsey/lib/snakemake_env.sh
 
 # Setup and run
 setup_snakemake
+
+# we need a custom snakemake command because the Pawsey GPU queue doesn't
+# accept the normal SBATCH arguments.
+(
+	XDG_CACHE_HOME="$(mktemp -d)" \
+		snakemake --profile profiles/pawsey \
+		--cluster-generic-submit-cmd "mkdir -p logs/slurm/{rule} && sbatch --time={resources.runtime} --partition=gpu --account=pawsey1132-gpu --gres=gpu:{resources.gpu} --job-name={rule}-smk --output=logs/slurm/{rule}/{rule}-%j.out --parsable" \
+		tiberius
+)
+
+exit 0
+
 run_snakemake annotation
